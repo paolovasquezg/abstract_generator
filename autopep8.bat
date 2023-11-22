45df0ecd3c303cb8f898a8247b6c0cd76edf83fd
paolovasquezg <paolo.vasquez@utec.edu.pe>
2023-11-21 20:16:10 -0500

    refresh

--- autopep8.bat
+++
@@ -1,2 +0,0 @@
-python -m autopep8 samila --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose --ignore=E721
-python -m autopep8 setup.py --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
